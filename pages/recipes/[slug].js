d704495e9d097611e0173466cb0284d00d9b4655
Lloyd Chambrier <lloydchambrier@gmail.com>
2021-06-30 18:29:22 +0200

    Fallback page added

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,6 +1,7 @@
 import {createClient} from 'contentful'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import Image from 'next/image'
+import Skeleton from '../../components/Skeleton'
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -18,7 +19,7 @@ export const getStaticPaths = async () => {
 
   return {
     paths,
-    fallback: false
+    fallback: true
   }
 }
 
@@ -34,6 +35,8 @@ export async function getStaticProps({ params }){
 }
 
 export default function RecipeDetails({recipe}) {
+  if(!recipe) return <Skeleton />
+  
   const { featuredImage, title, cookingTime, ingredients, methods } = recipe.fields
   //console.log(recipe.fields)
   return (
